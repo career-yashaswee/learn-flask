a69107d4ace3635fbcb1f167bd7fbd0810266c1f
career-yashaswee <career.yashaswee@gmail.com>
2026-01-20 13:35:08 +0530

    Section 5 73. Decorating responses with Flask-Smorest

--- resources/store.py
+++ resources/store.py
@@ -5,38 +5,41 @@ from db import stores
 from schemas import StoreSchema
 
 
-blp = Blueprint("stores", __name__, description="Operations on stores")
+blp = Blueprint("Stores", __name__, description="Operations on stores")
+
+
+@blp.route("/store/<string:store_id>")
+class Store(MethodView):
+    @blp.response(200, StoreSchema)
+    def get(cls, store_id):
+        try:
+            return stores[store_id]
+        except KeyError:
+            abort(404, message="Store not found.")
+
+    def delete(cls, store_id):
+        try:
+            del stores[store_id]
+            return {"message": "Store deleted."}
+        except KeyError:
+            abort(404, message="Store not found.")
 
 
 @blp.route("/store")
 class StoreList(MethodView):
-    def get(self):
-        return {"stores": list(stores.values())}
+    @blp.response(200, StoreSchema(many=True))
+    def get(cls):
+        return stores.values()
 
     @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
     def post(cls, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
-                abort(400, message="Store already exists.")
+                abort(400, message=f"Store already exists.")
 
         store_id = uuid.uuid4().hex
         store = {**store_data, "id": store_id}
         stores[store_id] = store
 
         return store
-
-
-@blp.route("/store/<string:store_id>")
-class Store(MethodView):
-    def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found.")
-
-    def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "Store deleted."}
-        except KeyError:
-            abort(404, message="Store not found.")
